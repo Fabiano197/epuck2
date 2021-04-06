2c78319e8e464f529cad5be50a0ebabd7f6c758c
Lukas Stuber <lukas.stuber5698@gmail.com>
2021-04-06 19:06:53 +0200

    In middle of TP5

--- TP5_Noisy/Noisy/audio_processing.c
+++ TP5_Noisy/Noisy/audio_processing.c
@@ -34,7 +34,7 @@ static float micBack_output[FFT_SIZE];
 *							so we have [micRight1, micLeft1, micBack1, micFront1, micRight2, etc...]
 *	uint16_t num_samples	Tells how many data we get in total (should always be 640)
 */
-void processAudioData(int16_t *data, uint16_t num_samples){
+void processAudioData(int16_t* data, uint16_t num_samples){
 
 	/*
 	*
@@ -43,6 +43,12 @@ void processAudioData(int16_t *data, uint16_t num_samples){
 	*	1024 samples, then we compute the FFTs.
 	*
 	*/
+	int isFull = 1;
+	while(isFull){
+		for(int i = 0; i < num_samples; i++){
+
+		}
+	}
 
 	
 }
--- TP5_Noisy/Noisy/main.c
+++ TP5_Noisy/Noisy/main.c
@@ -17,7 +17,7 @@
 #include <arm_math.h>
 
 //uncomment to send the FFTs results from the real microphones
-//#define SEND_FROM_MIC
+#define SEND_FROM_MIC
 
 //uncomment to use double buffering to send the FFT to the computer
 #define DOUBLE_BUFFERING
@@ -78,6 +78,7 @@ int main(void)
 
     /* Infinite loop. */
     while (1) {
+
 #ifdef SEND_FROM_MIC
         //waits until a result must be sent to the computer
         wait_send_to_computer();
@@ -95,11 +96,36 @@ int main(void)
 
         uint16_t size = ReceiveInt16FromComputer((BaseSequentialStream *) &SD3, bufferCmplxInput, FFT_SIZE);
 
+        static complex_float bufferCmplxInputConverted[FFT_SIZE];
+        static complex_float* bufferCmplxInputPointer = &bufferCmplxInputConverted[0];
+        float* loop_ptr = bufferCmplxInput;
+
+        for(uint16_t i = 0; i < FFT_SIZE; i++){
+        	bufferCmplxInputConverted[i].real = *loop_ptr;
+        	loop_ptr++;
+        	bufferCmplxInputConverted[i].imag = *loop_ptr;
+        	loop_ptr++;
+        }
+
         if(size == FFT_SIZE){
 
-            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
 
+            doFFT_optimized(FFT_SIZE, bufferCmplxInput);
+            //doFFT_c(FFT_SIZE, bufferCmplxInputPointer);
+
+
+            loop_ptr = bufferCmplxInput;
+            for(uint16_t i = 0; i < FFT_SIZE; i++){
+            	*loop_ptr = bufferCmplxInputConverted[i].real;
+            	loop_ptr++;
+            	*loop_ptr = bufferCmplxInputConverted[i].imag;
+            	loop_ptr++;
+			}
+
+            systime_t time_start = chVTGetSystemTime();
             arm_cmplx_mag_f32(bufferCmplxInput, bufferOutput, FFT_SIZE);
+			systime_t time_stop = chVTGetSystemTime();
+			chprintf((BaseSequentialStream *)&SDU1, "Time = %i ms \r\n", time_stop-time_start);
 
             SendFloatToComputer((BaseSequentialStream *) &SD3, bufferOutput, FFT_SIZE);
 
