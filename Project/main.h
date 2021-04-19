1658d97041338ccd1e8a8e0f878825333659b182
Lukas Stuber <lukas.stuber5698@gmail.com>
2021-04-19 22:06:13 +0200

    IR Sensors and Obstacle following

---
+++ Project/main.h
@@ -0,0 +1,21 @@
+#ifndef PROJECT_MAIN_H_
+#define PROJECT_MAIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "camera/dcmi_camera.h"
+#include "msgbus/messagebus.h"
+#include "parameter/parameter.h"
+
+/** Robot wide IPC bus. */
+extern messagebus_t bus;
+
+extern parameter_namespace_t parameter_root;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PROJECT_MAIN_H_ */
